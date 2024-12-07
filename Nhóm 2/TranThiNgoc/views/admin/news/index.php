71025f4f6b4f9d256ad1ac0f0e284e0d5f508567
TranNgocxX <152256291+TranNgocxX@users.noreply.github.com>
2024-12-07 16:15:58 +0700

    Update index.php

--- Nhóm 2/TranThiNgoc/views/admin/news/index.php
+++ Nhóm 2/TranThiNgoc/views/admin/news/index.php
@@ -8,31 +8,37 @@
     <h1> QUẢN LÝ TIN TỨC </h1>
     <a href="index.php?controller=news&action=add">Add News</a>
     <table border="1">
-        <thead>
-            <tr>
-                <th>Tiêu đề </th>
-                <th>Loại</th>
-                <th>Hành động </th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php if (!empty($newsList)): ?>
-                <?php foreach ($newsList as $news): ?>
-                    <tr>
-                        <td><?= $news['title'] ?></td>
-                        <td><?= $news['category_name'] ?></td>
-                        <td>
-                            <a href="index.php?controller=news&action=edit&id=<?= $news['id'] ?>">Sửa</a>
-                            <a href="index.php?controller=news&action=delete&id=<?= $news['id'] ?>" onclick="return confirm('Are you sure?')">Delete</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            <?php else: ?>
+    <thead>
+        <tr>
+            <th> ID </th>
+            <th> Tiêu đề </th>
+            <th> Nội dung </th> 
+            <th> Loại </th>
+            <th> Ngày tạo </th>
+            <th> Hành động </th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php if (!empty($newsList)): ?>
+            <?php foreach ($newsList as $news): ?>
                 <tr>
-                    <td colspan="3">No news available.</td>
+                    <td><?= htmlspecialchars($news['id']) ?></td>
+                    <td><?= htmlspecialchars($news['title']) ?></td>
+                    <td><?= htmlspecialchars($news['content']) ?></td>
+                    <td><?= htmlspecialchars($news['category_name']) ?></td>
+                    <td><?= htmlspecialchars($news['created_at']) ?></td>
+                    <td>
+                        <a href="index.php?controller=news&action=edit&id=<?= $news['id'] ?>">Sửa</a>
+                        <a href="index.php?controller=news&action=delete&id=<?= $news['id'] ?>" onclick="return confirm('Xác nhận xoá?')">Xoá</a>
+                    </td>
                 </tr>
-            <?php endif; ?>
-        </tbody>
-    </table>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <tr>
+                <td colspan="6"> No news available </td>
+            </tr>
+        <?php endif; ?>
+    </tbody>
+</table>
 </body>
 </html>
